c5e803503371db4a2345c1274f9202c8a59c5f3a
DAN <dantonfilipe.s@gmail.com>
2024-10-06 19:38:01 -0300

    raw: add OrderRepository file

--- src/main/java/Root/modules/OrderModel/database/repository/OrderRepository.java
+++ src/main/java/Root/modules/OrderModel/database/repository/OrderRepository.java
@@ -1,6 +1,6 @@
-package Root.Modules.OrderModel.database.repository;
+package Root.modules.OrderModel.database.repository;
 
-import Root.Modules.OrderModel.database.entity.OrderModel;
+import Root.modules.OrderModel.database.entity.OrderModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
