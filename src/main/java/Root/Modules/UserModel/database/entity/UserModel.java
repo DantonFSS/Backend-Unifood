6b05bf123098935634816e3e4d05eb95f0ab8f7e
DAN <dantonfilipe.s@gmail.com>
2024-10-06 19:34:29 -0300

    raw: add UserModel class file

--- src/main/java/Root/Modules/UserModel/database/entity/UserModel.java
+++ src/main/java/Root/Modules/UserModel/database/entity/UserModel.java
@@ -1,13 +1,13 @@
-package Root.Modules.UserModel.database.entity;
+package Root.modules.UserModel.database.entity;
 
+import Root.modules.AddressModel.database.entity.AddressModel;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.*;
 
+import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.UUID;
 
 @Table(name = "users")
 @Entity(name = "users")
@@ -15,38 +15,51 @@ import java.util.Date;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode(of = "cpf")
+@EqualsAndHashCode(of = "id")
 public class UserModel {
 
     @Id
-    @NotBlank(message = "O campo CPF não pode estar em branco.")
-    @Column(nullable = false, unique = true, length = 14, name = "user_cpf")
-    @Pattern(regexp = "^([0-9]{3}\\.[0-9]{3}\\.[0-9]{3}-[0-9]{2})$", message = "CPF inválido. O formato esperado é XXX.XXX.XXX-XX.")
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false, unique = true, name = "id_user")
+    private UUID id;
+
+    @Column(nullable = false, unique = true, length = 14, name = "cpf_user")
     private String cpf;
 
-
-    @NotBlank(message = "Name cannot be blank.")
-    @Column(nullable = false, length = 250, name = "user_name")
+    @Column(nullable = false, length = 250, name = "full_name_user")
     private String name;
 
-    /*
     @ManyToOne
-    @JoinColumn(name = "user_address", nullable = false)
+    @JoinColumn(name = "user_address")
     private AddressModel address;
-    */
 
     @Email
-    @NotBlank(message = "Name cannot be blank.")
-    @Column(length = 260, nullable = false, name = "user_email")
+    @Column(nullable = false, name = "user_email")
     private String email;
 
-    @Column(nullable = false, name = "birth_date_user")
-    private String birthDate;
+    @Column(name = "register_date_user", nullable = false, updatable = false)
+    private LocalDateTime userRegisterDate;
 
+    //@LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 
-    /*@Column(name = "register_date_user", nullable = false)
-    private Date userRegisterDate;*/
+    @Column(name = "password_user")
+    private String password;
 
-    //private UserRole role;
+    @Column(name = "phone_user")
+    private String phone;
 
+    @Column(name = "birth_date_user")
+    private Date birthDateUser;
+
+    @Column(name = "role_user")
+    private UserRole role;
+
+    public UserModel(String cpf, String name, String email, LocalDateTime userRegisterDate) {
+        this.cpf = cpf;
+        this.name = name;
+        this.email = email;
+        this.userRegisterDate = userRegisterDate;
+    }
 }
