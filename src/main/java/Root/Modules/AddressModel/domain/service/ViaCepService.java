95008560338e018359bec98fb3337e83d59417b2
DAN <dantonfilipe.s@gmail.com>
2024-10-20 18:35:41 -0300

    raw: Address - ViaCep service file

--- src/main/java/Root/Modules/AddressModel/domain/service/ViaCepService.java
+++ src/main/java/Root/Modules/AddressModel/domain/service/ViaCepService.java
@@ -1,6 +1,6 @@
-package Root.Modules.AddressModel.domain.service;
+package Root.modules.AddressModel.domain.service;
 
-import Root.Modules.AddressModel.database.entity.AddressModel;
+import Root.modules.AddressModel.database.entity.AddressModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
