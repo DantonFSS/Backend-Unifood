a17eef84fc4417a8a21d7ac57c698b8de2c73a79
DAN <dantonfilipe.s@gmail.com>
2024-10-23 03:35:09 -0300

    raw: add new UserResponseDto class file

--- src/main/java/Root/modules/users/domain/dtos/response/UserResponseDto.java
+++ src/main/java/Root/modules/users/domain/dtos/response/UserResponseDto.java
@@ -1,4 +1,4 @@
-package Root.modules.UserModel.domain.dtos.response;
+package Root.modules.users.domain.dtos.response;
 
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
@@ -6,19 +6,14 @@ import org.springframework.data.annotation.CreatedDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@ToString
-public class UserResponseDTO {
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @ToString
+public class UserResponseDto {
 
     private UUID id;
+    private String cpf;
     private String name;
     private String email;
     private String phone;
-
     @CreatedDate
     private LocalDateTime userRegisterDate;
-
 }
