d18491a35171b8aa6d158c2927e27d4713df23b0
DAN <dantonfilipe.s@gmail.com>
2024-10-23 03:44:47 -0300

    fix: remove 'USER_ALREADY_EXISTS' class attribute

--- src/main/java/Root/shared/common/utils/consts/UserErrorConstants.java
+++ src/main/java/Root/shared/common/utils/consts/UserErrorConstants.java
@@ -4,8 +4,6 @@ public class UserErrorConstants {
     public static final String CPF_ALREADY_EXISTS = "This user's \"cpf\"  already exists";
     public static final String CPF_DOES_NOT_MATCH = "Cpf does not match the format XXX.XXX.XXX-XX ";
     public static final String EMAIL_ALREADY_EXISTS = "This user's \"e-mail\"  already exists";
-    public static final String USER_ALREADY_EXISTS = "This user already exists";
     public static final String USER_NOT_FOUND = "This user was not found...";
     public static final String USER_NAME_NOT_FOUND = "This user's \"name\" is null";
-
 }
