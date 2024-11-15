c66be31d1307f8db9fb21441cdd8f5b10f810e7a
DAN <dantonfilipe.s@gmail.com>
2024-11-14 21:57:54 -0300

    .

--- src/main/java/Root/UnifoodApplication.java
+++ src/main/java/Root/UnifoodApplication.java
@@ -3,6 +3,7 @@ package Root;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class UnifoodApplication {
 
