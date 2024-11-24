82cc17cabce682ee4f0f3eb1a3f6dc7440b7d692
danton <dantonfilipe.s@gmail.com>
2024-11-23 23:01:38 -0300

    raw: add docker-compose file

--- src/main/java/Root/modules/items/infra/http/controller/CreateItemController.java
+++ src/main/java/Root/modules/items/infra/http/controller/CreateItemController.java
@@ -1,10 +1,12 @@
 package Root.modules.items.infra.http.controller;
 
 import Root.modules.items.domain.services.CreateItemService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Items", description = "Manage items - Create, Delete, Read and Update any item.")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/items")
