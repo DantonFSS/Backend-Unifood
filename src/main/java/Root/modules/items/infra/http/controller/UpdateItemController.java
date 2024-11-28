3a9bdd09f634b71d80675c7a578fe8fe6de0e02f
danton <dantonfilipe.s@gmail.com>
2024-11-28 04:48:12 -0300

    fix: rectify '@CrossOrigin' annotation as '(origins = 'http://localhost:address')'

--- src/main/java/Root/modules/items/infra/http/controller/UpdateItemController.java
+++ src/main/java/Root/modules/items/infra/http/controller/UpdateItemController.java
@@ -15,7 +15,7 @@ import java.util.UUID;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/items")
-//@CrossOrigin(origins = "http://localhost:8100")
+@CrossOrigin(origins = "http://localhost:8100")
 public class UpdateItemController {
 
     private final UpdateItemService iServ;
