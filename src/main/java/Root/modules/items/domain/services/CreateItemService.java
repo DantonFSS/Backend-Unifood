17baaf0f7347b4042e06a4276e3b5552f4e0ad9f
danton <dantonfilipe.s@gmail.com>
2024-11-29 00:20:54 -0300

    raw: add new info logged with slf4j

--- src/main/java/Root/modules/items/domain/services/CreateItemService.java
+++ src/main/java/Root/modules/items/domain/services/CreateItemService.java
@@ -2,11 +2,11 @@ package Root.modules.items.domain.services;
 
 import Root.modules.items.database.entity.ItemModel;
 import Root.modules.items.database.repository.ItemRepository;
-import Root.shared.common.utils.consts.ItemErrorConstants;
-import Root.shared.common.utils.exceptions.HttpBadRequestException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CreateItemService {
@@ -14,6 +14,7 @@ public class CreateItemService {
     private final ItemRepository iRepo;
 
     public ItemModel saveItem(ItemModel item) {
+        log.info("Saving item to database: {}", item);
         return iRepo.save(item);
     }
 
